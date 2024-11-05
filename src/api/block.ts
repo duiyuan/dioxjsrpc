20e54fe1c5fa9906ef4a75997010c261023fd98e
189 <189>
2024-11-05 18:00:02 +0800

    chore: add more utils

--- src/api/block.ts
+++ src/api/block.ts
@@ -1,23 +1,25 @@
-import provider from './provider'
 import Request, { DIOX } from './request'
 
 class BlockSvc extends Request {
-  getExcutedTx(params: { height: number; limit?: number; pos?: number }) {
-    const { limit = 10000, pos = 0, height } = params
+  async getExcutedTx(params: {
+    height: number
+    limit?: number
+    pos?: number
+  }): Promise<DIOX.ExcutedTx | undefined> {
+    const { limit = 500, pos = 0, height } = params
     const data = {
       limit,
       pos,
       height,
+      module: 'block',
+      action: 'txn_executed',
     }
-    const { dioxide } = provider.get()
-    const url = `${dioxide}/api?module=block&action=txn_executed`
-    return this.get<{
-      err?: number
-      rsp: string
-      ret: DIOX.Block
-    }>(url, {
+    const resp = await this.get<CommonResponse<DIOX.ExcutedTx>>('', {
       data,
     })
+    const { Status, Message, Result } = resp
+    if (Status) throw Message
+    return Result
   }
 }
 
--- src/api/overview.ts
+++ src/api/overview.ts
@@ -1,13 +1,16 @@
 import Request from './request'
 
 class OverviewService extends Request {
-  chainStatus() {
-    return this.get<DioxScanChainBasicInfo>('', {
+  async chainStatus() {
+    const resp = await this.get<DioxScanChainBasicInfo>('', {
       data: {
         module: 'chain',
         action: 'status',
       },
     })
+    const { Status, Message, Result } = resp
+    if (Status) throw Message
+    return Result
   }
 }
 
