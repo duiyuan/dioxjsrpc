4ef8d51cf7f46c315ab37f03a5556b0f5d421b03
lyylyylyylyy <lyyzju@outlook.com>
2025-04-02 14:26:11 +0800

    feat: webpack to make sdk run in browser env

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
     commonjs: true,
     amd: true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
